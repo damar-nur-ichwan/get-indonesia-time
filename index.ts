c85d965fed92d8ebd746f233f9b3ebf7f2c3e913
damar-nur-ichwan <damarnurichwan.official@gmail.com>
2022-10-01 08:56:58 +0700

    revision month : month + 1

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ export default function GetIndonesiaTime() {
   const localTimestamp = new Date(now.getTime());
   const day = localTimestamp.getDay();
   const date = localTimestamp.getDate();
-  const month = localTimestamp.getMonth();
+  const month = localTimestamp.getMonth() + 1;
   const year = localTimestamp.getFullYear();
   const hours = localTimestamp.getHours();
   const minutes = localTimestamp.getMinutes();
